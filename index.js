38012cdbf2472a58b2ee66fdc56b26507948d007
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-10 03:42:29 +0200

    Calc time using Date interface

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const iterativeFactorial = num => {
 }
 
 const recursiveFactorial = num => num === BigInt(1) ? BigInt(1) : num * recursiveFactorial(num - BigInt(1));
+
+const start = Date.now();
+recursiveFactorial(BigInt(10000))
+const end = Date.now();
+const timeInSeconds = (end - start) / 1000;
+console.log(`Excution time is: ${timeInSeconds}`);
