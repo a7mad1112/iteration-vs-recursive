e405fde4b3edab1633cd87b676195217f150b692
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-10 03:41:29 +0200

    Implement recursive algorithm

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const iterativeFactorial = num => {
   while (num != 1) factorial *= BigInt(num--);
   return factorial;
 }
+
+const recursiveFactorial = num => num === BigInt(1) ? BigInt(1) : num * recursiveFactorial(num - BigInt(1));
