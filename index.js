a34ad04c4709f30285f696437aade9f1c2f2937c
a7mad1112 <ahmalawneh79@gmail.com>
2023-11-10 03:48:20 +0200

    Function to calc max size of the stack

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const iterativeFactorial = num => {
 
 const recursiveFactorial = num => num === BigInt(1) ? BigInt(1) : num * recursiveFactorial(num - BigInt(1));
 
-const start = Date.now();
-recursiveFactorial(BigInt(10000))
-const end = Date.now();
-const timeInSeconds = (end - start) / 1000;
-console.log(`Excution time is: ${timeInSeconds}`);
+// const start = Date.now();
+// recursiveFactorial(BigInt(10000))
+// const end = Date.now();
+// const timeInSeconds = (end - start) / 1000;
+// console.log(`Excution time is: ${timeInSeconds}`);
+
+
+// var i = 0;
+// function inc() {
+//   i++;
+//   inc();
+// }
+// function calcMaxStackSize() {
+//   i++;
+//   try {
+//     inc();
+//   } catch (error) {
+//     i++;
+//     console.log(`Stack size is: ${i} in your runtime environment`);
+//   }
+// }
+// calcMaxStackSize();
